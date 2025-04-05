36455bf3c0b41250a4cef47ccafb8196f1d72521
gbxx1145 <78945571+gbxx1145@users.noreply.github.com>
2025-04-05 06:42:57 +0000

    support feature of fool day

---
+++ src/fool.ts
@@ -0,0 +1,5 @@
+export default function run(csImg: Il2Cpp.Image) {
+  csImg.class("Phigros2.Utils.AprilFool.AprilFool").method("get_EnableAprilFoolsFeatures").implementation = function () {
+    return true
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import "frida-il2cpp-bridge"
 import autoplay from "./autoplay"
 import chart_dump from "./chart_dump";
 import chart_replace from "./chart_replace";
+import fool from "./fool";
 
 Il2Cpp.perform(() => {
   const cs_img = Il2Cpp.domain.assembly("Assembly-CSharp").image
   const unity_cor_img = Il2Cpp.domain.assembly("UnityEngine.CoreModule").image
   const unity_json_serialize_img = Il2Cpp.domain.assembly("UnityEngine.JSONSerializeModule").image
+  fool(cs_img)
   autoplay(cs_img, unity_cor_img)
   chart_dump(cs_img, unity_json_serialize_img)
   chart_replace(cs_img, unity_cor_img)
